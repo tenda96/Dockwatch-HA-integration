9abc3ebd616d3bd0e74c748c63934892f3240503
tenda96 <luca96.tenderini@gmail.com>
2026-03-03 00:17:00 +0100

    Create const.py

---
+++ dockwatch/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Dockwatch integration."""
+from datetime import timedelta
+
+DOMAIN = "dockwatch"
+DEFAULT_PORT = 9999
+SCAN_INTERVAL = timedelta(minutes=1)
+
+# Configuration keys
+CONF_URL = "url"
