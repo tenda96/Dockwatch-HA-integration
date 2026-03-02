17a83e6cb9a0fee10448eba9ae501ab89088a1b2
tenda96 <luca96.tenderini@gmail.com>
2026-03-03 00:12:49 +0100

    Update config_flow.py
    update and refining

--- dockwatch/config_flow.py
+++ dockwatch/config_flow.py
@@ -1,22 +1,31 @@
 import logging
 import voluptuous as vol
-
 from homeassistant import config_entries
-from homeassistant.const import CONF_API_KEY
-from .sensor import CONF_DOCKER_CHECK_URL
+from homeassistant.const import CONF_API_KEY, CONF_HOST, CONF_PORT
 
 _LOGGER = logging.getLogger(__name__)
+DOMAIN = "dockwatch"
 
-class DockerConfigFlow(config_entries.ConfigFlow, domain="dockercheck"):
+class DockwatchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Dockwatch."""
+
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
         if user_input is None:
             return self.async_show_form(
                 step_id="user",
                 data_schema=vol.Schema(
                     {
-                        vol.Required(CONF_DOCKER_CHECK_URL): str,  
+                        vol.Required(CONF_HOST): str,
+                        vol.Required(CONF_PORT, default=9999): int,
                         vol.Required(CONF_API_KEY): str,
                     }
                 ),
             )
-        return self.async_create_entry(title="Docker Check", data=user_input)
+        
+        # Format the URL automatically
+        host = user_input[CONF_HOST]
+        port = user_input[CONF_PORT]
+        user_input["url"] = f"http://{host}:{port}"
+        
+        return self.async_create_entry(title=f"Dockwatch ({host})", data=user_input)
