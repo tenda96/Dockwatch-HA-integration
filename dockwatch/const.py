1796c8f5222289c604e94893106b1ec43c326ff6
tenda96 <luca96.tenderini@gmail.com>
2026-05-25 01:10:34 +0200

    Delete dockwatch directory

--- dockwatch/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for the Dockwatch integration."""
-from datetime import timedelta
-
-DOMAIN = "dockwatch"
-DEFAULT_PORT = 9999
-SCAN_INTERVAL = timedelta(minutes=1)
-
-# Configuration keys
-CONF_URL = "url"
