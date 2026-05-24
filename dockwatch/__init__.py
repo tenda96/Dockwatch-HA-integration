f8645d6caa5b2f1476324ac4df24a6020b73b46e
tenda96 <luca96.tenderini@gmail.com>
2026-05-24 23:24:17 +0200

    Update __init__.py

--- dockwatch/__init__.py
+++ dockwatch/__init__.py
@@ -13,8 +13,15 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Dockwatch from a config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+    
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Ricarica l'integrazione quando vengono salvate nuove opzioni."""
+    await hass.config_entries.async_reload(entry.entry_id)
